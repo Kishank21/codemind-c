d34aaf9f1eaa3d057c8e7ff1c3cdada592a025a4
Kishank21 <97622497+Kishank21@users.noreply.github.com>
2022-01-29 07:17:03 +0530

    #include<stdio.h> int main() {     int a,n,i=1;     scanf("%d%d",&a,&n);     while(i<=n)     {         printf("%d x %d = %d ",a,i,a*i);         i=i+2;     }     return 0; }

---
+++ Tables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,n,i=1;
+    scanf("%d%d",&a,&n);
+    while(i<=n)
+    {
+        printf("%d x %d = %d
+",a,i,a*i);
+        i=i+2;
+    }
+    return 0;
+}
